7da6ffdfbd922e220c5ba0b20efa5367fedb5f26
Sofia Jayaswal <sofia.p.jayaswal.25@dartmouth.edu>
2023-08-28 21:35:54 -0400

    trying to fix connection errors

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,70 +1,65 @@
 import fs from 'fs';
 import mysql from 'mysql2';
 import { Client } from 'ssh2';
-import {join, dirname} from 'path';
+import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-// Source: https://medium.com/@devontem/nodejs-express-using-ssh-to-access-mysql-remotely-60372832dd08
-// Create a new SSH client instance
 const ssh = new Client();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Create a promise to establish the SSH tunnel and MySQL connection
-const db = new Promise((resolve, reject) => {
-  // Set up an event listener for the 'ready' event
-  ssh.on('ready', () => {
-    // Forward the SSH connection to the MySQL server
-    ssh.forwardOut(
-      '127.0.0.1', // source address, usually any valid address
-      12345,      // source port, any valid port number
-      '127.0.0.1', // destination address (localhost on SSH server)
-      3306,       // destination port (MySQL default port)
-      async (err, stream) => {
-        if (err) {
-          // Reject the promise if there's an SSH error
-          reject(err);
-          return;
-        }
-
-        // Create a MySQL connection using the forwarded stream
-        const connection = await mysql.createConnection({
-          host: '127.0.0.1',
-          user: 'root',
-          password: 'SAQ7bmMdUW85cP',
-          database: 'cali_housing_prices',
-          stream,
-        });
-        // Connect to the MySQL database
-        connection.connect((err2) => {
-          if (err2) {
-            console.log('mysql connection error', err2)
-            // Reject the promise if there's a MySQL connection error
-            reject(err2);
-          } else {
-            // Resolve the promise if the connection is successful
-            resolve(connection);
+const connectToDatabase = () => {
+  return new Promise((resolve, reject) => {
+    ssh.on('ready', () => {
+      ssh.forwardOut(
+        '127.0.0.1',
+        12345,
+        '127.0.0.1',
+        3306,
+        async (err, stream) => {
+          if (err) {
+            reject(err);
+            return;
           }
-        });
-      },
-    );
-  });
 
-  // Set up an event listener for the 'error' event
-  ssh.on('error', (err) => {
-    console.log('ssh error', err)
-    // Reject the promise if there's an SSH error
-    reject(err);
-  });
+          const connection = await mysql.createConnection({
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'SAQ7bmMdUW85cP',
+            database: 'cali_housing_prices',
+            stream,
+          });
 
-  // Connect to the SSH server with the specified options
-  ssh.connect({
-    host: '18.222.24.247',
-    port: 22,
-    username: 'ubuntu',
-    privateKey: fs.readFileSync(join(__dirname, '../sofia_gc_key_final.pem')),
+          connection.connect((err2) => {
+            if (err2) {
+              console.log('mysql connection error', err2);
+              reject(err2);
+            } else {
+              resolve(connection);
+            }
+          });
+        }
+      );
+    });
+
+    ssh.on('error', (err) => {
+      console.log('ssh error', err);
+      // Retry the SSH connection after a delay (e.g., 5 seconds)
+      setTimeout(() => {
+        connectToDatabase().then(resolve).catch(reject);
+      }, 5000);
+    });
+
+    ssh.connect({
+      host: '18.222.24.247',
+      port: 22,
+      username: 'ubuntu',
+      privateKey: fs.readFileSync(join(__dirname, '../sofia_gc_key_final.pem')),
+    });
   });
-});
+};
+
+const db = connectToDatabase();
 
 export default db;
